27a7d4ca0116d7fda213c383fb1ce00eba8c4e0e
Yi-Chin Chuang <ychin.chuang@gmail.com>
2019-03-08 23:49:15 +0800

    Update run.py

--- hw/run.py
+++ hw/run.py
@@ -1,6 +1,5 @@
 # encoding: utf8
 
-# from download import text
 import requests, os
 import json
 import re
@@ -18,31 +17,6 @@ url = 'http://www.millionbook.net/wx/j/jingyong/tlbb/index.html'
 d1 = requests.get(url)
 d1.encoding = 'big5'
 titles = BeautifulSoup(d1.text, 'lxml').select('tr > td > a')
-
-# def download():
-#     for title in titles:
-#         titleurl = 'http://www.millionbook.net/wx/j/jingyong/tlbb/' + title.get('href')
-#         titlename = title.text
-#         # print(titlename, titleurl)
-        
-#         try:
-#             f = open(path + '%s.txt' % titlename, 'w')
-#             f.close()
-#         except:
-#             pass
-        
-#         d2 = requests.get(titleurl)
-#         d2.encoding = 'big5'
-#         contents = BeautifulSoup(d2.text, 'lxml').select('tr > td.tt2')
-        
-#         for content in contents:
-#             try:
-#                 f = open(path + '%s.txt' % titlename, 'w')
-#                 f.write(content.text)
-#                 f.close()
-#             except:
-#                 pass
-#         print(titlename + '-----[下載完成]')
 
 def main():
     # download()
